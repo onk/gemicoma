b36725d24d2feed0525300c33e4875b095223c6b
Takafumi ONAKA <takafumi.onaka@gmail.com>
2019-09-24 02:40:33 +0900

    Use sinatra-activerecord

---
+++ Rakefile
@@ -0,0 +1,7 @@
+require "sinatra/activerecord/rake"
+
+namespace :db do
+  task :load_config do
+    require File.join(__dir__, "app")
+  end
+end
--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require "bundler"
 Bundler.require(:default, ENV["RACK_ENV"] || "development")
 
 class App < Sinatra::Base
+  set :database_file, "config/database.yml"
+  register Sinatra::ActiveRecordExtension
+
   configure :development do
     register Sinatra::Reloader
   end
