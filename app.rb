8f10e8807288ba86d91ab5eb7921bd8650c1b42a
Takafumi ONAKA <takafumi.onaka@gmail.com>
2019-09-27 02:04:01 +0900

    CRUD projects

--- app.rb
+++ app.rb
@@ -8,10 +8,35 @@ Dir.glob("models/**/*").each { |f| require_relative f }
 Dir.glob("jobs/**/*").each { |f| require_relative f }
 
 class App < Sinatra::Base
+  set :method_override, true # POST _method hack
   set :database_file, "config/database.yml"
   register Sinatra::ActiveRecordExtension
 
   configure :development do
     register Sinatra::Reloader
+  end
+
+  get "/" do
+    @projects = Project.all
+    erb :"projects/index"
+  end
+
+  get "/projects/:id" do
+    @project = Project.find(params[:id])
+    erb :"projects/show"
+  end
+
+  post "/projects" do
+    project_params = {
+      name: params[:name]
+    }
+    Project.create!(project_params)
+    redirect "/"
+  end
+
+  delete "/projects/:id" do
+    project = Project.find(params[:id])
+    project.destroy!
+    redirect "/"
   end
 end
