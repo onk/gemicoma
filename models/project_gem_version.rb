7521161603227aaeffeb81e2e2214448183ed616
Takafumi ONAKA <takafumi.onaka@gmail.com>
2019-10-07 20:08:49 +0900

    Implements projects#show

--- models/project_gem_version.rb
+++ models/project_gem_version.rb
@@ -1,4 +1,24 @@
 class ProjectGemVersion < ActiveRecord::Base
   belongs_to :gem_version
   belongs_to :project
+
+  module Status
+    OUTDATED = 0
+    BEHIND   = 1
+    LATEST   = 2
+    UNKNOWN  = 3
+  end
+
+  def status
+    locked_version  = Gem::Version.new(locked_version)
+    current_version = Gem::Version.new(gem_version.version)
+
+    return Status::UNKNOWN unless locked_version && current_version
+    return Status::LATEST if current_version <= locked_version
+
+    # approximate_recommendation is "~> x.y.z"
+    # not satisfied by approximate_recommendation = major version is up = OUTDATED
+    locked_requirement = Gem::Requirement.new(locked_version.approximate_recommendation)
+    locked_requirement.satisfied_by?(current_version) ? Status::BEHIND : Status::OUTDATED
+  end
 end
