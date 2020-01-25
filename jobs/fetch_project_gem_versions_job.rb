30420bffe7878c89e1e9cd23fa9239ce0828dd34
Takafumi ONAKA <takafumi.onaka@gmail.com>
2020-01-26 02:11:13 +0900

    Fix indent

--- jobs/fetch_project_gem_versions_job.rb
+++ jobs/fetch_project_gem_versions_job.rb
@@ -2,25 +2,24 @@ class FetchProjectGemVersionsJob
   include Sidekiq::Worker
 
   def perform(args = {})
-      args = args.with_indifferent_access
-      project_id = args[:project_id]
+    args = args.with_indifferent_access
+    project_id = args[:project_id]
 
-      project = Project.find_by(id: project_id)
-      unless project
-        logger.warn "Unable to find project with id: #{project_id}"
-        return
-      end
+    project = Project.find_by(id: project_id)
+    unless project
+      logger.warn "Unable to find project with id: #{project_id}"
+      return
+    end
 
-      gemfile_lock_body = fetch_lockfile(project)
-      unless gemfile_lock_body
-        logger.warn "Unable to find Gemfile.lock for #{project.name}"
-        return
-      end
+    gemfile_lock_body = fetch_lockfile(project)
+    unless gemfile_lock_body
+      logger.warn "Unable to find Gemfile.lock for #{project.name}"
+      return
+    end
 
-      parser = Bundler::LockfileParser.new(gemfile_lock_body)
-      project.import_project_gem_versions(parser.specs)
+    parser = Bundler::LockfileParser.new(gemfile_lock_body)
+    project.import_project_gem_versions(parser.specs)
   end
-
 
   private
 
