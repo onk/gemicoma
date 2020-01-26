71eba15fb1e066664c0a9e74e4cb0e8142736c37
Takafumi ONAKA <takafumi.onaka@gmail.com>
2020-01-26 17:10:05 +0900

    Implement ProjectGemVersion#advisories

--- models/project_gem_version.rb
+++ models/project_gem_version.rb
@@ -21,4 +21,17 @@ class ProjectGemVersion < ActiveRecord::Base
     locked_requirement = Gem::Requirement.new(_locked_version.approximate_recommendation)
     locked_requirement.satisfied_by?(current_version) ? Status::BEHIND : Status::OUTDATED
   end
+
+  def self.advisory_database
+    @database ||= begin
+      root_path = File.expand_path("..", __dir__)
+      Bundler::Audit::Database.new(File.join(root_path, "tmp", "ruby-advisory-db"))
+    end
+  end
+
+  # database.checkgem require [name: String, version: Gem::Version] object
+  TempGem = Struct.new(:name, :version)
+  def advisories
+    @advisories ||= ProjectGemVersion.advisory_database.check_gem(TempGem.new(gem_version.name, Gem::Version.new(locked_version))).to_a
+  end
 end
